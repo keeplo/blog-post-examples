f4720ffe4681c41c538ad63a04345cdda7b65c3f
Keeplo <keepingitflow@gmail.com>
2023-05-13 16:42:42 +0900

    chore: 프로젝트 생성

---
+++ Pinch&Pan/Pinch&Pan/Pinch_PanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinch_PanApp.swift
+//  Pinch&Pan
+//
+//  Created by 김용우 on 2023/05/13.
+//
+
+import SwiftUI
+
+@main
+struct Pinch_PanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
